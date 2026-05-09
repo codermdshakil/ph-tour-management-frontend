1f772a2e3f42a4797847f52fc4c4a38398015dea
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-05-09 13:58:42 +0600

    feat: add Field and Separator components for improved form structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,41 @@
-
-import { createBrowserRouter } from "react-router"
-import App from "../App"
-import AdminLayout from "../components/layout/AdminLayout"
-import Analytics from "../components/layout/Analytics"
-import AboutPage from "../pages/AboutPage"
-import Login from "../pages/Login"
-import Register from "../pages/Register"
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import AdminLayout from "../components/layout/AdminLayout";
+import Analytics from "../components/layout/Analytics";
+import AboutPage from "../pages/AboutPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export const router = createBrowserRouter([
   {
     // element:<App/>,
-    Component:App,
-    path:"/",
-    children:[
+    Component: App,
+    path: "/",
+    children: [
       {
-        Component:AboutPage,
-        path:"about"
-      }
-    ]
+        Component: AboutPage,
+        path: "about",
+      },
+      
+    ],
   },
+
   {
-    Component:AdminLayout,
-    path:"/admin",
-    children:[
+    Component: AdminLayout,
+    path: "/admin",
+    children: [
       {
-        path:"analytics",
-        Component:Analytics
-      }
-    ]
+        path: "analytics",
+        Component: Analytics,
+      },
+    ],
   },
   {
-    Component:Login,
-    path:"/login"
+    Component: Login,
+    path: "/login",
   },
   {
-    Component:Register,
-    path:"/register"
-  }
-  
-])
+    Component: Register,
+    path: "/register",
+  },
+]);

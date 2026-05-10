31381217f0b5a36c45801c58e6c4bef720a89a30
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2026-05-10 12:42:24 +0600

    feat: integrate Redux for state management and add base API setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router";
 import "./index.css";
 import { ThemeProvider } from "./providers/theme.provider.tsx";
+import { store } from "./redux/store.ts";
 import { router } from "./routes/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>,
 );
